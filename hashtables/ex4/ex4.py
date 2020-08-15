6b78dc6bd3494784d6bfaa13a903dfb568f558d9
rdowd <rjarrettdowd@yahoo.com>
2020-08-14 21:22:56 -0400

    completed third

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,24 @@ def has_negatives(a):
     """
     # Your code here
 
+    #storage dict
+    storage ={}
+    #loop through a
+    for i in a:
+        new_num = i
+        if i < 0:
+            new_num = i * -1 
+        #if abs of entry is not in dict
+        if new_num not in storage: 
+            # add it as key with 1 as value
+            storage[new_num] = 1
+        else:
+            #increment value at abs of entry
+            storage[new_num] += 1
+
+    result = [k for k,v in storage.items() if v > 1]
+    
+    
     return result
 
 

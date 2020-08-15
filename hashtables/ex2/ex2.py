e1d8bd2c6487d96b0797f983af87c8d441fbd905
rdowd <rjarrettdowd@yahoo.com>
2020-08-14 20:35:47 -0400

    completed second

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,22 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    route = []
+    storage = {}
+    #loop through tickets
+    for i in tickets: 
+        #add each tickets source to a dict as a key and destination as value
+        storage[i.source] = i.destination
+    
+    #var dict[None]
+    current = storage['NONE']
+    index = 0
+    # while var is not none:
+    while index != length:
+        #append value to route
+        route.append(current)
+        #var = dict[var]
+        current = storage[current]
+        index += 1
 
     return route

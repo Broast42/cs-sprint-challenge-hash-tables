a5f82e2098730253e147dac64546bbecb2b4c3eb
rdowd <rjarrettdowd@yahoo.com>
2020-08-14 21:43:37 -0400

    completed fourth for stretch.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,7 +3,22 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
-
+    #storage dict
+    storage ={}
+    #loop through main array
+    for i in arrays:
+         #loop through array
+        for j in i:
+            #if entry not in dict
+            if j not in storage:
+                #add to dict with value of 1
+                storage[j] = 1
+            else:
+                #increment value at key
+                storage[j] += 1
+    
+    #loop through keys and grab all keys with values greater than 1
+    result = [k for k,v in storage.items() if v > 1]
     return result
 
 
